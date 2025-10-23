51ec9ce1af58275bf2e191fb8d70a3f779c09a99
Fabian Kleine <fabian.kleine@rhiem.com>
2025-10-23 14:40:10 +0200

    feat: setup next-intl and next-theme

---
+++ components/floating-buttons.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import { useCallback, useEffect, useState } from "react"
+import { useTheme } from 'next-themes'
+import { ThemeToggleButton, useThemeTransition } from "@/components/ui/shadcn-io/theme-toggle-button"
+import { Button } from "./ui/button";
+import useCookie from "@/hooks/use-cookie";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export function FloatingButtons() {
+    const router = useRouter();
+
+    const { resolvedTheme, setTheme } = useTheme();
+    const { startTransition } = useThemeTransition()
+    const [mounted, setMounted] = useState(false);
+
+    const [locale, setLocale] = useCookie<string>('locale', 'en', 365);
+
+    useEffect(() => {
+        setMounted(true)
+    }, []);
+
+    const handleThemeToggle = useCallback(() => {
+        const newTheme = resolvedTheme === 'dark' ? 'light' : 'dark'
+
+        startTransition(() => {
+            setTheme(newTheme)
+        })
+    }, [resolvedTheme, setTheme, startTransition]);
+
+    if (!mounted) {
+        return null;
+    }
+
+    return (
+        <div className="fixed bottom-4 right-4 z-50 flex items-center justify-center flex-col gap-2">
+            <Button
+                variant="outline"
+                size="icon"
+                className="p-2"
+                onClick={() => {
+                    const newLocale = locale === 'en' ? 'de' : 'en';
+                    setLocale(newLocale);
+                    router.refresh();
+                }}
+            >
+                <Image
+                    src={locale === 'en' ? '/de.png' : '/gb.png'}
+                    alt="Locale"
+                    className="object-cover h-full w-full rounded-full"
+                    width={24}
+                    height={16}
+                />
+            </Button>
+            <ThemeToggleButton
+                theme={resolvedTheme === 'dark' ? 'dark' : 'light'}
+                onClick={handleThemeToggle}
+                variant="circle"
+                start="bottom-right"
+            />
+        </div>
+    );
+}
--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,37 +1,40 @@
+"use client"
+
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
     Field,
-    FieldDescription,
     FieldGroup,
     FieldLabel,
-    FieldSeparator,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import { useTranslations } from "next-intl"
 
 export function LoginForm({
     className,
     ...props
 }: React.ComponentProps<"form">) {
+    const t = useTranslations("LoginForm");
+
     return (
         <form className={cn("flex flex-col gap-6", className)} {...props}>
             <FieldGroup>
                 <div className="flex flex-col items-center gap-1 text-center">
-                    <h1 className="text-2xl font-bold">Login to your account</h1>
+                    <h1 className="text-2xl font-bold">{t('title')}</h1>
                     <p className="text-muted-foreground text-sm text-balance">
-                        Enter your email below to login to your account
+                        {t('subtitle')}
                     </p>
                 </div>
                 <Field>
-                    <FieldLabel htmlFor="email">Email</FieldLabel>
-                    <Input id="email" type="email" placeholder="m@example.com" required />
+                    <FieldLabel htmlFor="email">{t('emailLabel')}</FieldLabel>
+                    <Input id="email" type="email" placeholder={t('emailPlaceholder')} required />
                 </Field>
                 <Field>
-                    <FieldLabel htmlFor="password">Password</FieldLabel>
+                    <FieldLabel htmlFor="password">{t('passwordLabel')}</FieldLabel>
                     <Input id="password" type="password" required />
                 </Field>
                 <Field>
-                    <Button type="submit">Login</Button>
+                    <Button type="submit">{t('loginButton')}</Button>
                 </Field>
             </FieldGroup>
         </form>
