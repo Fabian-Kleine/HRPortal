d2fc65db2e2627efb0dafcafe3a45ca13f9567da
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2025-11-24 20:08:07 +0100

    feat: enhance FloatingButtons with locale toggle functionality

--- components/floating-buttons.tsx
+++ components/floating-buttons.tsx
@@ -7,15 +7,20 @@ import { Button } from "./ui/button";
 import useCookie from "@/hooks/use-cookie";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { ButtonGroup } from "./ui/button-group";
+import { Globe } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 export function FloatingButtons() {
     const router = useRouter();
+    const t = useTranslations("FloatingButtons");
 
     const { resolvedTheme, setTheme } = useTheme();
     const { startTransition } = useThemeTransition()
     const [mounted, setMounted] = useState(false);
+    const [localeMenuOpen, setLocaleMenuOpen] = useState(false);
 
-    const [locale, setLocale] = useCookie<string>('locale', 'en', 365);
+    const [_, setLocale] = useCookie<string>('locale', 'en', 365);
 
     useEffect(() => {
         setMounted(true)
@@ -33,26 +38,59 @@ export function FloatingButtons() {
         return null;
     }
 
+    function handleLocaleToggle(locale: string) {
+        setLocale(locale);
+        setLocaleMenuOpen(false);
+        router.refresh();
+    }
+
     return (
         <div className="fixed bottom-4 right-4 z-50 flex items-center justify-center flex-col gap-2">
-            <Button
-                variant="outline"
-                size="icon-lg"
-                className="p-2"
-                onClick={() => {
-                    const newLocale = locale === 'en' ? 'de' : 'en';
-                    setLocale(newLocale);
-                    router.refresh();
-                }}
-            >
-                <Image
-                    src={locale === 'en' ? '/de.png' : '/gb.png'}
-                    alt="Locale"
-                    className="object-cover h-full w-full rounded-full"
-                    width={36}
-                    height={36}
-                />
-            </Button>
+            <ButtonGroup orientation="vertical">
+                {localeMenuOpen && (
+                    <>
+                        <Button
+                            variant="outline"
+                            size="icon-lg"
+                            className="p-2"
+                            onClick={() => handleLocaleToggle('en')}
+                        >
+                            <Image
+                                src={'/gb.png'}
+                                alt="English"
+                                className="object-cover h-full w-full rounded-full"
+                                width={36}
+                                height={36}
+                            />
+                            <span className="sr-only">Change language to English</span>
+                        </Button>
+                        <Button
+                            variant="outline"
+                            size="icon-lg"
+                            className="p-2"
+                            onClick={() => handleLocaleToggle('de')}
+                        >
+                            <Image
+                                src={'/de.png'}
+                                alt="Deutsch"
+                                className="object-cover h-full w-full rounded-full"
+                                width={36}
+                                height={36}
+                            />
+                            <span className="sr-only">Sprache auf Deutsch ändern</span>
+                        </Button>
+                    </>
+                )}
+                <Button
+                    variant="outline"
+                    size="icon-lg"
+                    className="p-2"
+                    onClick={() => setLocaleMenuOpen(!localeMenuOpen)}
+                >
+                    <Globe />
+                    <span className="sr-only">{t('changeLanguage')}</span>
+                </Button>
+            </ButtonGroup>
             <ThemeToggleButton
                 theme={resolvedTheme === 'dark' ? 'dark' : 'light'}
                 onClick={handleThemeToggle}
